ad82e1e0a076f24fa1f56526a69cc27bb321b593
Oshanath Rajawasam <oshanath@wso2.com>
2025-08-06 00:02:30 +0530

    Add Azure connector for Gateway Federation.

---
+++ azure/components/azure.gw.manager/src/main/java/org/wso2/azure/client/AzureGatewayConfiguration.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2025 WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.azure.client;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.annotations.Component;
+import org.wso2.azure.client.util.AzureAPIUtil;
+import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.api.model.ConfigurationDto;
+import org.wso2.carbon.apimgt.api.model.GatewayAgentConfiguration;
+import org.wso2.carbon.apimgt.api.model.GatewayPortalConfiguration;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * This class contains the configurations related to Azure Gateway
+ */
+@Component(
+        name = "azure.external.gateway.configuration.component",
+        immediate = true,
+        service = GatewayAgentConfiguration.class
+)
+public class AzureGatewayConfiguration implements GatewayAgentConfiguration {
+    private static final Log log = LogFactory.getLog(AzureAPIUtil.class);
+
+    @Override
+    public String getImplementation() {
+        return AzureGatewayDeployer.class.getName();
+    }
+
+    @Override
+    public List<ConfigurationDto> getConnectionConfigurations() {
+        List<ConfigurationDto> configurationDtoList = new ArrayList<>();
+        configurationDtoList.add(new ConfigurationDto("tenant_id", "Tenant ID", "input",
+                "Directory (tenant) ID of your Microsoft Entra ID.", "", true, false,
+                Collections.emptyList(), false));
+        configurationDtoList.add(new ConfigurationDto("subscription_id", "Subscription ID", "input",
+                "Azure subscription GUID that owns the APIM instance.", "", true, false,
+                Collections.emptyList(), false));
+        configurationDtoList.add(new ConfigurationDto("client_id", "Client ID", "input",
+                "Application (client) ID of the service principal used for Azure authentication.", "", true,
+                false, Collections.emptyList(), false));
+        configurationDtoList.add(new ConfigurationDto("client_secret", "Client Secret", "input",
+                "Password/secret created for the service principal.", "", true, true,
+                Collections.emptyList(), false));
+        configurationDtoList.add(new ConfigurationDto("resource_group", "Resource Group", "input",
+                "The Azure resource group name containing the API Management service.", "", true, false,
+                Collections.emptyList(), false));
+        configurationDtoList.add(new ConfigurationDto("service_name", "APIM Service Name", "input",
+                "The name of the Azure API Management service resource.", "", true, false,
+                Collections.emptyList(), false));
+
+        return configurationDtoList;
+    }
+
+    @Override
+    public String getType() {
+        return AzureConstants.AZURE_TYPE;
+    }
+
+    @Override
+    public GatewayPortalConfiguration getGatewayFeatureCatalog() throws APIManagementException {
+        try (InputStream inputStream = AzureGatewayConfiguration.class.getClassLoader()
+                .getResourceAsStream(AzureConstants.GATEWAY_FEATURE_CATALOG_FILENAME)) {
+
+            if (inputStream == null) {
+                throw new APIManagementException("Gateway Feature Catalog JSON not found");
+            }
+
+            // Initialize Gson
+            Gson gson = new Gson();
+
+            InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+
+            JsonObject gatewayObject = jsonObject.getAsJsonObject(AzureConstants.AZURE_TYPE);
+
+            List<String> apiTypes = gson.fromJson(gatewayObject.get("apiTypes"),
+                    new TypeToken<List<String>>() {}.getType());
+            JsonObject gatewayFeatures = gatewayObject.get("gatewayFeatures").getAsJsonObject();
+
+            GatewayPortalConfiguration config = new GatewayPortalConfiguration();
+            config.setGatewayType(AzureConstants.AZURE_TYPE);
+            config.setSupportedAPITypes(apiTypes);
+            config.setSupportedFeatures(gatewayFeatures);
+
+            return config;
+        } catch (Exception e) {
+            throw new APIManagementException("Error occurred while reading Gateway Feature Catalog JSON", e);
+        }
+    }
+
+    @Override
+    public String getDefaultHostnameTemplate() {
+        return AzureConstants.AZURE_API_EXECUTION_URL_TEMPLATE;
+    }
+}
---
+++ azure/components/azure.gw.manager/src/main/java/org/wso2/azure/client/util/GatewayUtil.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2025 WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.azure.client.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.api.model.API;
+
+public class GatewayUtil {
+
+    private static final Pattern VALID_PATH_PATTERN = Pattern.compile("^[a-zA-Z0-9-._~%!$&'()*+,;=:@/]*$");
+
+    public static String getEndpointURL(API api) throws APIManagementException {
+
+        try {
+            String endpointConfig = api.getEndpointConfig();
+            if (StringUtils.isEmpty(endpointConfig)) {
+                return endpointConfig;
+            }
+            JSONParser parser = new JSONParser();
+            JSONObject endpointConfigJson = null;
+
+            endpointConfigJson = (JSONObject) parser.parse(endpointConfig);
+
+            JSONObject prodEndpoints = (JSONObject)endpointConfigJson.get("production_endpoints");
+            String productionEndpoint = (String) prodEndpoints.get("url");
+
+            return productionEndpoint.charAt(productionEndpoint.length() - 1) == '/' ?
+                    productionEndpoint.substring(0, productionEndpoint.length() - 1) : productionEndpoint;
+        } catch (ParseException e) {
+            throw new APIManagementException("Error while parsing endpoint configuration", e);
+        }
+    }
+
+    public static String validateAzureAPIEndpoint(String urlString) {
+        try {
+            if (StringUtils.isEmpty(urlString)) {
+                return null;
+            }
+            URL url = new URL(urlString);
+
+            // Validate scheme (only http and https are allowed)
+            String protocol = url.getProtocol();
+            if (!"http".equalsIgnoreCase(protocol) && !"https".equalsIgnoreCase(protocol)) {
+                return "Invalid Endpoint URL";
+            }
+
+            // Validate host
+            if (url.getHost() == null || url.getHost().isEmpty()
+                    || url.getHost().equalsIgnoreCase("localhost")) {
+                return "Invalid Endpoint URL";
+            }
+
+            // Validate path (no illegal characters)
+            if (!VALID_PATH_PATTERN.matcher(url.getPath()).matches()) {
+                return "Invalid Endpoint URL";
+            }
+            return null;
+        } catch (MalformedURLException e) {
+            return "Invalid Endpoint URL";
+        }
+    }
+}
